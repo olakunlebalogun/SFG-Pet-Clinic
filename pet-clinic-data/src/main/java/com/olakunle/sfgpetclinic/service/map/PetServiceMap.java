a71d1f92d668965cdb88274e5878544d8218d7be
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-08-06 15:13:43 +0100

    Created the map service for each entity. closes #6

---
+++ pet-clinic-data/src/main/java/com/olakunle/sfgpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.olakunle.sfgpetclinic.service.map;
+
+import com.olakunle.sfgpetclinic.models.Pet;
+import com.olakunle.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public Pet save(Pet obj) {
+        return super.save(obj.getId(), obj);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/olakunle/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.olakunle.sfgpetclinic.service.map;
+
+import com.olakunle.sfgpetclinic.models.Vet;
+import com.olakunle.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public Vet save(Vet obj) {
+        return super.save(obj.getId(), obj);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
