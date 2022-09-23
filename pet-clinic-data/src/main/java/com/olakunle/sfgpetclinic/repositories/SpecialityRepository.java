73b62e8e5110c78f3b607868ecf07c9b3dab1f2d
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-09-23 11:15:35 +0100

    need to test the github action

--- pet-clinic-data/src/main/java/com/olakunle/sfgpetclinic/repositories/SpecialityRepository.java
+++ pet-clinic-data/src/main/java/com/olakunle/sfgpetclinic/repositories/SpecialityRepository.java
@@ -4,4 +4,6 @@ import com.olakunle.sfgpetclinic.models.Speciality;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+
+
 }
